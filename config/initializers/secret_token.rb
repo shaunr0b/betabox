2d4746bba734afdcd17e4557369d1dbb11f4d74c
Shaun Robinson <shaun@squiid.com>
2012-04-18 20:11:22 +0530

    Bootstrap, Backbone, LESS, Slim installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Betabox::Application.config.secret_token = 'e498f7240cd8c216d117e0ade60d1d8145cb6eefbf889a0ac0cff8828e3d3d1b9935a980d4b5f23ceb6042cc48e4a5bb53c6919e0408cb050449b7e2b1378232'
