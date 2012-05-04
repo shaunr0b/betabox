1799d6f90edc21b03ae433df850d5f90f576204a
Shaun Robinson <shaun@squiid.com>
2012-05-04 11:13:39 +0200

    Updated config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Betabox::Application.initialize!
+
+
+unless Rails.env.production?
+  ENV['SALT1'] = %Q(3g&4uLZ8-Ph'r"E2PG@1<3!wGQ3P#K)
+end
